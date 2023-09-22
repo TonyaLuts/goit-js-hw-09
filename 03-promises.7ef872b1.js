ea755fa5cad3f82e8e16edac97ebf3ed232a90bf
TonyaLuts <131724958+TonyaLuts@users.noreply.github.com>
2023-09-22 18:17:27 +0000

    Deploying to gh-pages from @ TonyaLuts/goit-js-hw-09@81567de06fb1eca63154dfb183699f6b1a862326 🚀

--- 03-promises.7ef872b1.js
+++
@@ -1,2 +0,0 @@
-const o=new Date;console.log(o);const n=Date.now();console.log(n);
-//# sourceMappingURL=03-promises.7ef872b1.js.map
--- 03-promises.a03afdf9.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=new Date;console.log(o);var n=Date.now();console.log(n)}();
-//# sourceMappingURL=03-promises.a03afdf9.js.map
